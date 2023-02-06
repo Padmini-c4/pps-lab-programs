5275ce2d5704487a792b0050c0d6ae9d6063ce27
Padmini-c4 <120702795+Padmini-c4@users.noreply.github.com>
2023-02-06 16:20:49 +0530

    Add files via upload

---
+++ all_operators.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int a=10,b=60;
+printf("sum of the numbers is %d\n",a+b);
+printf("diference of the numbers is %d\n",a-b);
+printf("product of the numbers is %d\n",a*b);
+printf("division of the numbers is %d\n",a/b);
+printf("mode of the numbers is %d\n",a%b);
+printf("relational 1:%d\n",a>b);
+printf("relational 2:%d\n",a<b);
+printf("relational 3:%d\n",a==b);
+printf("relational 4:%d\n",a!=b);
+printf("relational 3:%d\n",a==b);
+printf("relational 3:%d\n",a==b);
+printf("bitwise and:%d\n",a>b&&b>a);
+printf("bitwise or:%d\n",a/b);
+printf("bitwise exclusive or:%d\n",a^b);
+printf("logical not:%d\n",!a>b);
+printf("logical and:%d\n",a<b&a>b);
+printf("right shift of a:%d\n",a>>2);
+printf("left shift of b:%d\n",b<<1);
+printf("increment of a:%d\n",a++);
+printf("decrement of b:%d\n",--b);
+return;
+}
